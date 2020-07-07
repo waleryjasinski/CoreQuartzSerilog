07702cd48579b46ba8fed49776ecca9fe05c368f
szerem <szerem@go2.pl>
2020-07-07 15:11:55 +0200

    solution

---
+++ CoreQuartzSerilogService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoreQuartzSerilogService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
